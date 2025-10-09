8c7d62bd7ca9e7fc286d91b5631dba823d5cd425
iirmaalaaww <intan.nirmalasari68@sma.belajar.id>
2025-10-09 19:24:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ tombolSapa.addEventListener('click', function() {
   // 3. Jalankan kode ini ketika tombol di-klik
   alert('Halo! Terima kasih sudah berkunjung!');
 });
+
+// 1. Select the button and the body
+const themeToggleButton = document.getElementById('tombol-tema');
+const bodyElement = document.body;
+// 2. Add a 'click' event Listener to the button
+themeToggleButton.addEventListener('click', function() {
+    // 3. Toggle the 'dark-mode' class on the body
+    bodyElement.classList.toggle('dark-mode');
+});
