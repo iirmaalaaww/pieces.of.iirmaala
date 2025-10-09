c1c54b77457b4a452967ecda26fb2f09f6c6372e
iirmaalaaww <intan.nirmalasari68@sma.belajar.id>
2025-10-09 19:30:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-  // 3. Jalankan kode ini ketika tombol di-klik
-  alert('Halo! Terima kasih sudah berkunjung!');
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Halo! Terima kasih sudah berkunjung!');
 });
 
 // 1. Select the button and the body
 const themeToggleButton = document.getElementById('tombol-tema');
 const bodyElement = document.body;
-// 2. Add a 'click' event Listener to the button
+// 2. Add a 'click' event listener to the button
 themeToggleButton.addEventListener('click', function() {
     // 3. Toggle the 'dark-mode' class on the body
     bodyElement.classList.toggle('dark-mode');
